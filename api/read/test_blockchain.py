f635f8732e59e12a6ce16eaf6b9e12922ec5d271
smypmsa <antonsauchyk@gmail.com>
2025-05-25 10:39:50 +0000

    feat: add blockchain for tests

---
+++ api/read/test_blockchain.py
@@ -0,0 +1,25 @@
+from common.metrics_handler import BaseVercelHandler, MetricsHandler
+from config.defaults import MetricsServiceConfig
+from metrics.solana import (
+    HTTPGetBalanceLatencyMetric,
+    HTTPGetBlockLatencyMetric,
+    HTTPGetProgramAccsLatencyMetric,
+    HTTPGetRecentBlockhashLatencyMetric,
+    HTTPGetTxLatencyMetric,
+    HTTPSimulateTxLatencyMetric,
+)
+
+metric_name = f"{MetricsServiceConfig.METRIC_PREFIX}response_latency_seconds"
+
+METRICS = [
+    (HTTPGetRecentBlockhashLatencyMetric, metric_name),
+    (HTTPSimulateTxLatencyMetric, metric_name),
+    (HTTPGetBalanceLatencyMetric, metric_name),
+    (HTTPGetBlockLatencyMetric, metric_name),
+    (HTTPGetTxLatencyMetric, metric_name),
+    (HTTPGetProgramAccsLatencyMetric, metric_name),
+]
+
+
+class handler(BaseVercelHandler):
+    metrics_handler = MetricsHandler("TEST_BLOCKCHAIN", METRICS)

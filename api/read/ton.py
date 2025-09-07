bb76bfc390bdc24fcfb568e039a0c8511ad334e1
smypmsa <antonsauchyk@gmail.com>
2025-09-07 19:59:37 +0000

    chore(ton): remove san fran region

--- api/read/ton.py
+++ api/read/ton.py
@@ -13,7 +13,7 @@ from metrics.ton import (
 METRIC_NAME = f"{MetricsServiceConfig.METRIC_PREFIX}response_latency_seconds"
 ALLOWED_REGIONS: list[str] = [
     "fra1",  # Frankfurt (EU)
-    "sfo1",  # San Francisco (US West)
+    # "sfo1",  # San Francisco (US West)
     "sin1",  # Singapore
     # "kix1",  # Osaka (JP)
     # "hnd1", "Tokyo" (JP)
